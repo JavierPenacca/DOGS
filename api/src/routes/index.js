a493fe1803026ef83a38734e01b5b9b07646ca97
JavierPenacca <javier.penacca@gmail.com>
2023-07-25 14:21:59 +0100

    API

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,22 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const dogsHandler = require("../handlers/dogsHandler");
+const dogByIdHandler = require("../handlers/dogsByIdHandler");
+const dogPostHandler = require("../handlers/dogPostHandler");
+const temperamentsHandler = require("../handlers/temperamentsHandler");
 
 
 const router = Router();
+
+
+router.get("/dogs", dogsHandler);
+
+router.get("/dogs/:id", dogByIdHandler);
+
+router.post("/dogs", dogPostHandler);
+
+router.get("/temperaments", temperamentsHandler);
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
