b08bc4c9c67e5270281c765efe05db8a024d7e97
JavierPenacca <javier.penacca@gmail.com>
2023-07-26 18:40:51 +0100

    ultima hora

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,6 +5,7 @@ const dogsHandler = require("../handlers/dogsHandler");
 const dogByIdHandler = require("../handlers/dogsByIdHandler");
 const dogPostHandler = require("../handlers/dogPostHandler");
 const temperamentsHandler = require("../handlers/temperamentsHandler");
+// const deleteHandler = require('../handlers/deleteDog');
 
 
 const router = Router();
@@ -17,6 +18,8 @@ router.get("/dogs/:id", dogByIdHandler);
 router.post("/dogs", dogPostHandler);
 
 router.get("/temperaments", temperamentsHandler);
+
+// router.delete("/dogs", deleteHandler);
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
